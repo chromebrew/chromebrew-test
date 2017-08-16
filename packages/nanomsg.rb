04bf677c415d58621b307373c750cd57b5f675c0
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-16 09:24:25 +0900

    Change nanomsg.rb to fix version string (, to .)

--- packages/nanomsg.rb
+++ packages/nanomsg.rb
@@ -3,7 +3,7 @@ require 'package'
 class Nanomsg < Package
   description 'nanomsg is a socket library that provides several common communication patterns.'
   homepage 'http://nanomsg.org/'
-  version '1.0,0'
+  version '1.0.0'
   source_url 'https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz'
   source_sha256 '24afdeb71b2e362e8a003a7ecc906e1b84fd9f56ce15ec567481d1bb33132cc7'
 
